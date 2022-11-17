a19f3b6aef66260d81b9e3632aad83faf305c099
Shantanu <97823352+ShawnMaz@users.noreply.github.com>
2022-11-17 10:47:00 -0500

    added comments to the App.js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './Components/Nav';
 import {useState} from 'react';
 
 function App() {
+  // Nav bar links
   const [navLinks] = useState([
     {
       name:'About Me'
@@ -17,7 +18,10 @@ function App() {
       name:'Resume'
     }
   ]);
+
+  // Default state of the Nav bar
   const [currentNavLink, setCurrentNavLink] = useState(navLinks[0]);
+  
   return (
     <div>
       <Nav navLinks = {navLinks} currentNavLink={currentNavLink} setCurrentNavLink={setCurrentNavLink}></Nav>
