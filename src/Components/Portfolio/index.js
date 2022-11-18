4f5841574f9c5aa86c22a2ce8b806a798b780556
Shantanu <97823352+ShawnMaz@users.noreply.github.com>
2022-11-18 16:41:22 -0500

    added more projects to the Portfolio

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -23,6 +23,27 @@ function Portfolio(){
             deployedUrl:'https://shawnmaz.github.io/weather-forecast/',
             githubRepo:'https://github.com/ShawnMaz/weather-forecast',
             techStack:'HTML, Bootstrap, JavaScript'
+        },
+        {
+            name:'Coding Quiz',
+            image:'coding-quiz.png',
+            deployedUrl:'https://shawnmaz.github.io/coding-quiz/',
+            githubRepo:'https://github.com/ShawnMaz/coding-quiz',
+            techStack:'HTML, CSS & JavaScript'
+        },
+        {
+            name:'Social Network API',
+            image:'social-network-api.png',
+            deployedUrl:'https://drive.google.com/file/d/1Y7hzS__JwlnRsbdZLG6xfrHL7OpiVMf3/view?usp=sharing',
+            githubRepo:'https://github.com/ShawnMaz/SocialNetworkAPI',
+            techStack:'Express, MongoDB & Node'
+        }, 
+        {
+            name:'E-commerce API',
+            image:'ecommerce-api.png',
+            deployedUrl:'https://drive.google.com/file/d/1gXRhRjEjAcsXV_fE73UtN_qVNpVQqsJZ/view?usp=sharing',
+            githubRepo:'https://github.com/ShawnMaz/E-commerce',
+            techStack:'MySQL, Express & Node'
         }
     ]);
     return(
