e0ac9eec12a33ae7034a5e94772d82f18d52626f
Shantanu <97823352+ShawnMaz@users.noreply.github.com>
2022-11-21 14:36:49 -0500

    added iframe to show the resume on page. Added style for the iframe

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -3,10 +3,11 @@ import resume from './resume.pdf';
 function Resume(){
 
     return(
-        // <section>
-        //     <a href={resume} download>Click</a>
-        //     <embed src={resume}/>
-        // </section>
+        <section className='container'>
+            <div className='d-flex flex-column mt-3'>
+                <iframe title='myResume' src={resume}></iframe>
+            </div>
+        </section>
     );
 }
 
