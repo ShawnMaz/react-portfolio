bd103401ae6b206957cd5d6d32ee9acd7329b747
Shantanu <97823352+ShawnMaz@users.noreply.github.com>
2022-11-21 15:04:55 -0500

    removed iframe from the resume due to rendering issues in the mobile. Added front and back end competencies to the resume page

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -4,8 +4,30 @@ function Resume(){
 
     return(
         <section className='container'>
-            <div className='d-flex flex-column mt-3'>
-                <iframe title='myResume' src={resume}></iframe>
+            <div className='row'>
+                <div className='col-sm-12 mx-auto my-3'>
+                    <p><a href={resume} className='text-primary' download>Click here</a> to download the resume.</p>
+                </div>
+                <div className='col-sm-12'>
+                    <h2>Front-end Profeciencies</h2>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Responsive design</li>
+                        <li>React</li>                        
+                        <li>Bootstrap</li>                        
+                    </ul>
+                </div>
+                <div className='col-sm-12'>
+                    <h2>Back-end Profeciencies</h2>
+                    <ul>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>MySQL, Sequelize</li>
+                        <li>MongoDB, Mongoose</li>
+                        <li>REST</li>
+                    </ul>
+                </div>
             </div>
         </section>
     );
